48305ebc0ad3ffd808b928da21a10d795fd5348e
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-05-27 21:57:17 +0630

    Testing system spec

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+ let!(:task) { FactoryBot.create(:task, title: 'task')}
+ let!(:task_order) { FactoryBot.create(:task, title: 'task')}
+ before do
+   visit tasks_path
+ end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -15,12 +20,6 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
-        visit tasks_path
-        binding.irb
-        current_path
-        Task.count
-        page.html
         expect(page).to have_content 'task'
       end
     end
